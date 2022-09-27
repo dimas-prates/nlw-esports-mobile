9bc0965cf05b6a7bac5c7c634b5c60fb7b7f786b
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-09-26 22:38:02 -0300

    :sparkles: feat(screen - home): implement API using effects + states and remove game utils

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,22 @@
 import { View, Image, FlatList } from "react-native";
-
 import { styles } from "./styles";
 import logoImage from "../../assets/logo-nlw-esports.png";
 import { Heading } from "../../components/Heading";
-import { GameCard } from "../../components/GameCard";
-import { GAMES } from "../../utils/games";
+import { GameCard, GameCardProps } from "../../components/GameCard";
+import { useEffect, useState } from "react";
+
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+  useEffect(() => {
+    fetch("http://192.168.15.26:3333/games")
+      .then(response => response.json())
+      .then(data => setGames(data));
+  }, []);
   return (
     <View style={styles.container}>
       <Image source={logoImage} style={styles.logo} />
       <Heading title="Find your duo!" subtitle="Select the game you want to play" />
-      <FlatList data={GAMES} keyExtractor={item => item.id} renderItem={({ item }) => (
+      <FlatList data={games} keyExtractor={item => item.id} renderItem={({ item }) => (
         <GameCard data={item} />
       )} horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.contentList} />
 
