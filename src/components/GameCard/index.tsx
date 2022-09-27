694b716b146270e570ac28ca0f3eaf1395b61ad0
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-09-26 22:36:44 -0300

    :sparkles: feat(component - gamecard): adjust type and component to the data received from the API

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -5,9 +5,9 @@ import { LinearGradient } from "expo-linear-gradient";
 import { THEME } from "../../theme";
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType;
+  title: string;
+  _count: { ads: number };
+  bannerUrl: string;
 }
 interface Props extends TouchableOpacityProps {
   data: GameCardProps;
@@ -15,13 +15,13 @@ interface Props extends TouchableOpacityProps {
 export function GameCard({ data, ...rest }: Props) {
   return (
     <TouchableOpacity style={styles.container}{...rest}>
-      <ImageBackground source={data.cover} style={styles.cover} >
+      <ImageBackground source={{ uri: data.bannerUrl }} style={styles.cover} >
         <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
           <Text style={styles.name}>
-            {data.name}
+            {data.title}
           </Text>
           <Text style={styles.ads}>
-            {data.ads}  Notifications
+            {data._count.ads}  Notifications
           </Text>
         </LinearGradient>
       </ImageBackground>
