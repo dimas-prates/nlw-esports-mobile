37302c5190bfbab826a46ceca39e02990b3c83b1
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-09-27 23:35:20 -0300

    :sparkles: feat(home - index.tsx): change "view" for "safe area view" and add background

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,25 +1,35 @@
-import { View, Image, FlatList } from "react-native";
+import { Image, FlatList } from "react-native";
 import { styles } from "./styles";
 import logoImage from "../../assets/logo-nlw-esports.png";
 import { Heading } from "../../components/Heading";
 import { GameCard, GameCardProps } from "../../components/GameCard";
 import { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Background } from "../../components/Background";
+import { useNavigation } from "@react-navigation/native";
 
 export function Home() {
   const [games, setGames] = useState<GameCardProps[]>([]);
+  const navigation = useNavigation();
+
+  function handleOpenGame({ id, title, bannerUrl }: GameCardProps) {
+    navigation.navigate("game", { id, title, bannerUrl });
+  }
+
   useEffect(() => {
     fetch("http://192.168.15.26:3333/games")
       .then(response => response.json())
       .then(data => setGames(data));
   }, []);
   return (
-    <View style={styles.container}>
-      <Image source={logoImage} style={styles.logo} />
-      <Heading title="Find your duo!" subtitle="Select the game you want to play" />
-      <FlatList data={games} keyExtractor={item => item.id} renderItem={({ item }) => (
-        <GameCard data={item} />
-      )} horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.contentList} />
-
-    </View>
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <Image source={logoImage} style={styles.logo} />
+        <Heading title="Find your duo!" subtitle="Select the game you want to play" />
+        <FlatList data={games} keyExtractor={item => item.id} renderItem={({ item }) => (
+          <GameCard data={item} onPress={() => handleOpenGame(item)} />
+        )} horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.contentList} />
+      </SafeAreaView>
+    </Background>
   );
 }
