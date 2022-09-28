dff598f0945603f9038772a8fe4e50902e30f0aa
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-09-27 23:38:59 -0300

    :recycle: refactor(routes - index.tsx): remove unused import (react)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import React, { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { AppRoutes } from "./app.routes";
 
 export function Routes() {
