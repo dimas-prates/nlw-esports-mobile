ab3d66bf7108d5721e0c004d5207f7b28bf2e8e0
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-09-28 22:16:14 -0300

    :recycle: refactor(gamecard - index.tsx): remove unused import (react)

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,17 +1,19 @@
-import React, { TouchableOpacity, TouchableOpacityProps, ImageBackground, ImageSourcePropType, Text } from "react-native";
-
+import { TouchableOpacity, TouchableOpacityProps, ImageBackground, ImageSourcePropType, Text } from "react-native";
 import { styles } from "./styles";
 import { LinearGradient } from "expo-linear-gradient";
 import { THEME } from "../../theme";
+
 export interface GameCardProps {
   id: string;
   title: string;
   _count: { ads: number };
   bannerUrl: string;
 }
+
 interface Props extends TouchableOpacityProps {
   data: GameCardProps;
 }
+
 export function GameCard({ data, ...rest }: Props) {
   return (
     <TouchableOpacity style={styles.container}{...rest}>
@@ -20,6 +22,7 @@ export function GameCard({ data, ...rest }: Props) {
           <Text style={styles.name}>
             {data.title}
           </Text>
+
           <Text style={styles.ads}>
             {data._count.ads}  Notifications
           </Text>
