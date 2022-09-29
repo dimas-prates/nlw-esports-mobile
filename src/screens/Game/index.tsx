ae17ccf522f9778990b14ab2ae516c258cda69d8
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-09-28 22:18:07 -0300

    :sparkles: feat(screen - game): add double cards view using flatlist, text, useEffect and useStates

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -3,20 +3,30 @@ import { styles } from "./styles";
 import { Background } from "../../components/Background";
 import { useRoute, useNavigation } from "@react-navigation/native";
 import { GameParams } from "../../@types/navigation";
-import { View, TouchableOpacity, Image } from "react-native";
+import { View, TouchableOpacity, Image, FlatList, Text } from "react-native";
 import { Entypo } from "@expo/vector-icons";
 import { THEME } from "../../theme";
 import logoImg from "../../assets/logo-nlw-esports.png";
 import { Heading } from "../../components/Heading";
+import { DuoCard, DuoCardProps } from "../../components/DuoCard";
+import { useEffect, useState } from "react";
 
 export function Game() {
+
   const route = useRoute();
   const game = route.params as GameParams;
   const navigation = useNavigation();
+  const [duos, setDuos] = useState<DuoCardProps[]>([]);
 
   function handleGoBack() {
     navigation.goBack();
   }
+
+  useEffect(() => {
+    fetch(`http://192.168.15.26:3333/games/${game.id}/ads`)
+      .then(response => response.json())
+      .then(data => setDuos(data));
+  }, []);
 
   return (
     <Background>
@@ -28,8 +38,25 @@ export function Game() {
           <Image source={logoImg} style={styles.logo} />
           <View style={styles.right} />
         </View>
+
         <Image source={{ uri: game.bannerUrl }} style={styles.cover} resizeMode="cover" />
+
         <Heading title={game.title} subtitle="Log in and start playing!" />
+        <FlatList data={duos}
+          keyExtractor={item => item.id}
+          // eslint-disable-next-line @typescript-eslint/no-empty-function
+          renderItem={({ item }) => (<DuoCard data={item} onConnect={() => { }} />)}
+          style={styles.containerList}
+          horizontal
+          contentContainerStyle={[duos.length > 0 ? styles.contentList : styles.emptyListContent]}
+          showsHorizontalScrollIndicator={false}
+
+          ListEmptyComponent={() => (
+            <Text style={styles.emptyListText}>
+              There&apos;s no alerts yet.
+            </Text>
+          )} />
+
       </SafeAreaView>
     </Background>
   );
